ca013a16f24ef5f880066b30a04ce43e4ad89b80
Arvid Nilsson <arvid2.nilsson@gmail.com>
2025-05-28 08:37:41 +0200

    Fix bug with proliferation of spaces

--- external/ImGuiColorTextEdit/TextEditor.cpp
+++ external/ImGuiColorTextEdit/TextEditor.cpp
@@ -105,7 +105,8 @@ std::string TextEditor::GetText(const Coordinates& aStart, const Coordinates& aE
         } else {
             istart = 0;
             ++lstart;
-            result += '\n';
+            if (lstart < lend)
+                result += '\n';
         }
     }
 
