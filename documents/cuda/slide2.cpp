e0951bfc30c768ee090a453f016f80e0b9a1c38b
Arvid Nilsson <arvid2.nilsson@gmail.com>
2025-06-08 19:26:23 +0200

    Add some latex & plot here

--- documents/cuda/slide2.cpp
+++ documents/cuda/slide2.cpp
@@ -1,5 +1,15 @@
 int blocks = 1024/256; int threads = 256;
 igubanit<<<blocks, threads>>>(d_A, dim);
-float *d_A_host = (float *)malloc(dim*sizeof(float));
-cudaMemcpy(d_A_host, d_A, dim * sizeof(float), cudaMemcpyDeviceToHost);
-d_A_host[42]
+float *d_AA_host = (float *)malloc(dim*sizeof(float));
+cudaMemcpy(d_AA_host, d_A, dim * sizeof(float), cudaMemcpyDeviceToHost);
+d_AA_host[42];
+
+auto update = [&]() {
+	ImGui::Text("Hej! %f", d_AA_host[42]);
+	ImGui::Latex(R"(\alpha \wedge \beta)");
+	if (ImPlot::BeginPlot("Hej")) {
+		ImPlot::EndPlot();
+	}
+};
+
+return update;
